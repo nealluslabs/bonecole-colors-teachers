c4322082ef02366d623486cae43ac721b5f37993
khalid <yusufkasim83@gmail.com>
2023-08-29 20:20:33 +0100

    updated logo

--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -53,6 +53,7 @@ export default function Nav({ openNav, onCloseNav }) {
         height: 1,
         '& .simplebar-content': { height: 1, display: 'flex', flexDirection: 'column' },
       }}
+    >
       <Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }}>{/* <Logo /> */}</Box>
       <Box sx={{ mb: 5, mx: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Link underline="none">
